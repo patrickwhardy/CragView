5a211189b651b53c917718ed102de81d46640857
patrickwhardy <patrickwhardy@gmail.com>
2016-06-06 21:52:18 -0600

    setup google+ oauth

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  def self.from_omniauth(auth)
+    where(uid: auth[:uid]).first_or_create do |user|
+      user.uid = auth.uid
+      user.provider = auth.provider
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      # user.save!
+    end
+  end
+
+
+
+end
