71314592421ffec74e3267d6c12ac6aada81e628
patrickwhardy <patrickwhardy@gmail.com>
2016-06-08 17:05:52 -0600

    setup mocha

--- spec/features/user_creates_crags_spec.rb
+++ spec/features/user_creates_crags_spec.rb
@@ -1,9 +1,12 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.feature "User can add a crag" do
   context "with their photospheres" do
     it "gets photospheres" do
       user = create(:user)
+      ApplicationController.any_instance.stubs(:current_user).returns(user)
+
+
     end
   end
 end
