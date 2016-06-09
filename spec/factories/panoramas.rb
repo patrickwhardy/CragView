3d595e33721d7ada17393eb7785653c3ca96a0aa
patrickwhardy <patrickwhardy@gmail.com>
2016-06-09 12:16:18 -0600

    write feature test

--- spec/factories/panoramas.rb
+++ spec/factories/panoramas.rb
@@ -1,7 +1,7 @@
-# FactoryGirl.define do
-#   factory :panorama do
-#     location "MyString"
-#     user nil
-#     crag nil
-#   end
-# end
+FactoryGirl.define do
+  factory :panorama do
+    location "40.0132937,-105.3091995"
+    user User.first
+    crag nil
+  end
+end
--- spec/features/user_creates_crags_spec.rb
+++ spec/features/user_creates_crags_spec.rb
@@ -4,18 +4,27 @@ RSpec.feature "User can add a crag" do
   context "with their photospheres" do
     it "gets photospheres" do
       user = create(:user)
-      # panorama = create(:panorama)
+      panorama = create(:panorama)
 
       ApplicationController.any_instance.stubs(:current_user).returns(user)
 
       visit(dashboard_path(user))
 
-      # click on image
-      # fill in crag name with _
-      # fill in routes/description/directions
-      # click create crag
-      # expect current path to be crag#show
-      # expect it to have crag data
+      click_on panorama.location
+
+      expect(current_path).to eq(new_crag_path)
+
+      fill_in "Name", with: "Practice Rock"
+      fill_in "Description", with: "A great place to learn the ropes."
+      fill_in "Directions", with: "Drive west of boulder until you see some big rocks."
+
+      click_on "Create Crag"
+
+      expect(current_path).to eq(crag_path(Crag.last))
+
+      expect(page).to have_content("Practice Rock")
+      expect(page).to have_content("A great place to learn the ropes.")
+      expect(page).to have_content("Drive west of boulder until you see some big rocks.")
     end
   end
 end
